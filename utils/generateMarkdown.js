ae770f58d6281435964a5f02e50641b6e337b53f
Herman <evanaherman@gmail.com>
2023-01-24 22:52:13 -0800

    updated markdown js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,9 +14,46 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [License](#license)
+  - [Questions](#questions)  
+
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contribution}
+
+  ## Testing
+
+  ${data.test}
+
+  ## License
+
+  
+
+  ## Questions
+
+  For any questions regarding this project, please contact the developer at: ${email}
+
+  You may also visit the developer's Github profile here: (https://github.com/${github})
+
 `;
 }
-
-
 
 module.exports = generateMarkdown;
